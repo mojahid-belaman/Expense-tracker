e49641e3d8c873cbc9dd12ed826315f386578deb
mojahid-belaman <mojahidbelaman@gmail.com>
2024-02-20 17:03:43 +0100

    create: custom widget chart

---
+++ lib/widget/chart/chart.dart
@@ -0,0 +1,79 @@
+import 'package:expense_tracker/modals/expense.dart';
+import 'package:expense_tracker/widget/chart/chart_bar.dart';
+import 'package:flutter/material.dart';
+
+class Chart extends StatelessWidget {
+  const Chart({super.key, required this.expenses});
+
+  final List<Expense> expenses;
+
+  List<ExpenseBucket> get buckets {
+    return [
+      ExpenseBucket.forCategory(expenses, Category.food),
+      ExpenseBucket.forCategory(expenses, Category.leisure),
+      ExpenseBucket.forCategory(expenses, Category.travel),
+      ExpenseBucket.forCategory(expenses, Category.work),
+    ];
+  }
+
+  double get maxTotalExpense {
+    double maxTotalExpense = 0;
+
+    for (final bucket in buckets) {
+      if (bucket.totalExpenses > maxTotalExpense) {
+        maxTotalExpense = bucket.totalExpenses;
+      }
+    }
+
+    return maxTotalExpense;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(8),
+      padding: const EdgeInsets.symmetric(horizontal: 7, vertical: 14),
+      width: double.infinity,
+      height: 180,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        gradient: LinearGradient(
+          colors: [
+            Theme.of(context).colorScheme.primary.withOpacity(0.3),
+            Theme.of(context).colorScheme.primary.withOpacity(0.0)
+          ],
+          begin: Alignment.bottomCenter,
+          end: Alignment.topCenter,
+        ),
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                for (final bucket in buckets)
+                  ChartBar(
+                      fill: bucket.totalExpenses == 0
+                          ? 0
+                          : bucket.totalExpenses / maxTotalExpense),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            children: buckets
+                .map(
+                  (bucket) => Expanded(
+                    child: Icon(
+                      categoryIcon[bucket.category],
+                    ),
+                  ),
+                )
+                .toList(),
+          )
+        ],
+      ),
+    );
+  }
+}
