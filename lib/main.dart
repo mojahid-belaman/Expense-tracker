ff50ab00f727ed6c9b8cf9d43f8f3dc10a0b7116
mojahid-belaman <mojahidbelaman@gmail.com>
2024-02-21 14:51:21 +0100

    do somthing like disable landiscape to loock nice app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widget/expenses.dart';
+import 'package:flutter/services.dart';
 
 final kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
@@ -10,6 +11,10 @@ final kDarkColorScheme = ColorScheme.fromSeed(
     seedColor: const Color.fromARGB(255, 5, 99, 125));
 
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]).then((fn) => {
   runApp(
     MaterialApp(
       // TODO - Dark Mode
